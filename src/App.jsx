4564482502868ffd087c348db5c1bc6453925f69
Adarsh goswami <adarshgoswami221206@gmail.com>
2026-01-19 11:32:16 +0530

    compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
+import { useState } from 'react'
+import './App.css' 
+
 function App() {
+
+  let [counter , setcounter] = useState(0)
+  function Advalue(){
+    let head = document.querySelectorAll("h1");
+    setcounter(counter = counter + 1);
+    console.log(counter);
+  }
+  function reValue(){
+    if(counter == 0) {
+      
+      return ;
+    }
+    setcounter(counter = counter - 1);
+    console.log(counter);
+  }
   return (
     <>
+    <h1>Hello Free Fire Players</h1>
+    <button
+    onClick={Advalue}>Count: {counter}</button>
+    <br></br>
+    <button
+    onClick = {reValue} >Count: {counter}</button>
     </>
   )
 }
